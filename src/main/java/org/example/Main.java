dc104d980a1097d3f5e104b9b993ee3ef2bca2fd
Daniel Imbrea <danelthedev@gmail.com>
2024-03-27 22:21:20 +0200

    implemented encryption

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Base64;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte[] word = "1111111111111111".getBytes();
+        byte[] key  = "1111111111111111".getBytes();
+
+        AESEncryptor aesEncryptor = new AESEncryptor();
+
+        byte[] encryptedWord = aesEncryptor.cipher(word, key);
+        // convert byte array to base 64 string
+        String encryptedWordBase64 = Base64.getEncoder().encodeToString(encryptedWord);
+        System.out.println("Encrypted word: " + encryptedWordBase64);
     }
 }
